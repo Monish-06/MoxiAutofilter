c67a223715334a3716f3053d89777bf642a3f228
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-24 20:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import threading
 from pyrogram import Client
 from keep_alive import keep_alive
-from handlers.index_handler import index_router
+from handlers.index_handler import *
+
 
 app = Client("MoxiBot", api_id=int(os.environ["API_ID"]),
              api_hash=os.environ["API_HASH"],
              bot_token=os.environ["BOT_TOKEN"])
 
-app.add_handler(index_router)
 
 if __name__ == "__main__":
     keep_alive()  # Start Flask keep-alive
