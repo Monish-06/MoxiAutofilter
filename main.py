60a246ad0173821d6931f530153711c5bf774e5e
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-24 14:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-import asyncio
 import os
 from pyrogram import Client
 from dotenv import load_dotenv
-import threading
-import requests
-import time
 
-# Load environment variables
 load_dotenv()
 
-# === Keep-Alive Function ===
-def keep_alive():
-    while True:
-        try:
-            requests.get("https://scornful-andreana-moxi35-c66f799a.koyeb.app/")
-        except:
-            pass
-        time.sleep(90)
+plugins = dict(root="handlers")
 
-# Start keep-alive in background thread
-threading.Thread(target=keep_alive, daemon=True).start()
-
-# === Bot Setup ===
-bot = Client(
-    "MoxiFilterBot",
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH")),
+app = Client(
+    "MoxiBot",
     bot_token=os.getenv("BOT_TOKEN"),
-    plugins={"root": "handlers"},
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    plugins=plugins
 )
 
 if __name__ == "__main__":
-    print("Starting MoxiFilterBot...")
-    bot.run()
+    from keep_alive import keep_alive
+    keep_alive()
+    print("Bot is running...")
+    app.run()
