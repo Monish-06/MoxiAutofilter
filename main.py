426e7d89b9273949ccccbe548331601c2cc0d364
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-24 19:09:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-from flask import Flask
-import threading
-from pyrogram import Client
 import os
-from keep_alive import keep_alive  # Import the keep_alive function from keep_alive.py
+from dotenv import load_dotenv
+from pyrogram import Client
+from keep_alive import keep_alive
 
-# Initialize Flask app
-app = Flask(__name__)
+load_dotenv()
 
-# Define a health check route
-@app.route("/health", methods=["GET"])
-def health_check():
-    return "OK", 200  # Koyeb will check this endpoint
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Run Flask server in a separate thread to keep it running alongside the bot
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)  # Ensure Flask listens on port 8080
-
-# Initialize your Pyrogram bot
-def run_bot():
-    bot = Client("my_bot", api_id="23448804", api_hash="07a3c7834916cb7a4318026dbf0eac28", bot_token="7818209258:AAHYKSNMaHMj035mxi0IotbEA5LRSxystDo")
-    bot.run()  # Start the bot
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins={"root": "handlers"})
 
 if __name__ == "__main__":
-    # Start Flask server in the background
-    threading.Thread(target=run_flask, daemon=True).start()
-
-    # Start keep_alive in a separate thread
-    threading.Thread(target=keep_alive, daemon=True).start()
-
-    # Start the bot
-    run_bot()
+    keep_alive()
+    app.run()
