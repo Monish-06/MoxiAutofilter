e1a16fb77c459d24c5843770104443779db2bd74
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-24 20:49:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import threading
 from pyrogram import Client
 from keep_alive import keep_alive
 from handlers.index_handler import *
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This loads the .env file
 
 
 app = Client("MoxiBot", api_id=int(os.environ["API_ID"]),
