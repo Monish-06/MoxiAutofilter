0ba11a54ead8cf6248486e671f49180c4dbfd040
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-28 09:05:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pyrogram import Client
 from keep_alive import keep_alive
 from dotenv import load_dotenv
 from handlers.index_handler import register_handlers
+from handlers.filter_handler import *
 import os
 
 load_dotenv()
